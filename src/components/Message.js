a012afc72e02e06dab8af7e8917ed597f0243e86
Hoai Tin <20130436@st.hcmuaf.edu.vn>
2023-06-14 09:51:43 +0700

    fomat date

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,6 +1,8 @@
 import "../sass/Message.scss"
 import {useEffect, useState} from "react";
 import {Emoji, EmojiStyle} from "emoji-picker-react";
+import {fomatDate} from "../Dao/Fomat";
+
 function Message(props){
     const [isIcon,setIsIcon] = useState( false);
     const [listIcon,setListIcon] = useState([]);
@@ -13,10 +15,12 @@ function Message(props){
         }
     },[
     ])
+    const date = fomatDate(props.createAt);
     return(
+
         <div className={`message ${props.myMessage === true ? "myMessage": ""}`}>
             <div className={`message_sender ${props.myMessage === true ? "disNone" : ""}`}>
-                {props.name}-{props.createAt}
+                {props.name}-<div>{date}</div>
             </div>
             <div className={`message_value  ${props.myMessage === true ? "message_right": ""}
             `}> {isIcon===true&&listIcon.length>0?<div> {listIcon.map((item,index)=>{
